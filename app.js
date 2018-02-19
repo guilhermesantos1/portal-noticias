5b247d3b45cefdbd996147be651929093f37416b
Guilherme Santos <guilherme.santos@tlsbr.com>
2018-02-19 12:35:39 -0300

    refactorando o codigo add mysql sem conexao ainda

--- app.js
+++ app.js
@@ -1,21 +1,8 @@
-var express = require('express');
-var app = express();
-
-app.set('view engine', 'ejs');
-
-app.get('/', function(req, res){
-	res.render('home/index');
-});
-
-app.get('/add_form_noticia', function(req, res){
-	res.render('admin/form_add_noticia');
-});
-
-app.get('/noticias', function(req, res){
-	res.render('noticias/noticias');
-});
-
+var app = require('./config/server.js');
+var home = require('./app/routes/home')(app);
+var rotaNoticias = require('./app/routes/noticias')(app);
+var formularioInclusaoNoticias = require('./app/routes/formulario_inclusao_noticia')(app);
 
 app.listen(3000, function(){
-	console.log("server rodando com Express na porta >>> 3000");
+	console.log("Server ON!!! Porta: 3000");
 });
