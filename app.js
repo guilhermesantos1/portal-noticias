356236db83cbbd67bc294718d20567bc64201ad8
Guilherme Santos <guilherme.santos@tlsbr.com>
2018-02-19 16:32:31 -0300

    add consign como dependencia

--- app.js
+++ app.js
@@ -1,7 +1,4 @@
 var app = require('./config/server.js');
-var home = require('./app/routes/home')(app);
-var rotaNoticias = require('./app/routes/noticias')(app);
-var formularioInclusaoNoticias = require('./app/routes/formulario_inclusao_noticia')(app);
 
 app.listen(3000, function(){
 	console.log("Server ON!!! Porta: 3000");
--- config/dbConnection.js
+++ config/dbConnection.js
@@ -1,10 +1,23 @@
-var mysql = require('mysql');
+/*
+*   Para conectar no banco de dados através do PowerShell do windows ou cmd
+*   startar o serviço, depois procurar o arquivo .exe do mysql
+*   ir no cmd ou PowerShell e depois que estiver no diretório bin/
+*   digitar mysql -u root -p
+*   colocar a senha e então conectar ao mysql server
+*/
 
-module.exports = function() {
+var mysql = require('mysql');
+var connMySQL = function() {
+    console.log("estabeleci uma conexao com o bd");
     return mysql.createConnection({
         host: 'localhost',
         user: 'root',
         password: '1234',
         database: 'portal_noticias'
     });
+}
+
+module.exports = function() {
+    console.log("autoload carregou o modulo de conn com o bd");
+    return connMySQL;
 };
