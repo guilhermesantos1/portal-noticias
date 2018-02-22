c7838ead6d6c75df248916a03ca2bd77c9652965
Guilherme Santos <guilherme.santos@tlsbr.com>
2018-02-22 13:43:59 -0300

    fix bud nas imagens na route /noticias/salvar... e att views

--- app/routes/admin.js
+++ app/routes/admin.js
@@ -3,7 +3,7 @@ module.exports =  function(application) {
         application.app.controllers.admin.formulario_inclusao_noticia(application, req, res);
     });
 
-    application.post('/noticias/salvar', function(req, res){
+    application.post('/noticias_salvar', function(req, res){
         application.app.controllers.admin.noticias_salvar(application, req, res);
     });
 };
