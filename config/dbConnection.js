74d9712dbd70eb535cf392e30a46a75547324d08
Guilherme Santos <guilherme.santos@tlsbr.com>
2018-08-08 14:50:44 -0300

    excluindo comentario

--- config/dbConnection.js
+++ config/dbConnection.js
@@ -1,11 +1,3 @@
-/*
-*   Para conectar no banco de dados através do PowerShell do windows ou cmd
-*   startar o serviço, depois procurar o arquivo .exe do mysql
-*   ir no cmd ou PowerShell e depois que estiver no diretório bin/
-*   digitar mysql -u root -p
-*   colocar a senha e então conectar ao mysql server
-*/
-
 var mysql = require('mysql');
 
 var connMySQL = function() {
